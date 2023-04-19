92bdce6eabda624168b8cd69a60d3accdfd00818
Toran Bruce Richards <toran.richards@gmail.com>
2023-04-19 15:52:31 +1200

    Outputs the top 5 closest matches, filters out prs.

--- duplicate_issue_locator.py
+++ duplicate_issue_locator.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 import gensim
 from gensim.parsing.preprocessing import remove_stopwords
 import matplotlib.pyplot as plt
+from heapq import nlargest
 
 def fetch_open_issues(owner, repo, since_days=30):
     """Fetch all open issues from the specified GitHub repository."""
@@ -15,7 +16,7 @@ def fetch_open_issues(owner, repo, since_days=30):
     per_page = 100
     since_date = (datetime.now() - timedelta(days=since_days)).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    headers = {'Authorization': 'token YOUR_PERSONAL_ACCESS_TOKEN'}
+    headers = {'Authorization': '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'}
 
     while True:
         url = f'https://api.github.com/repos/{owner}/{repo}/issues?state=open&per_page={per_page}&page={page}&author={owner}&since={since_date}'
@@ -40,7 +41,7 @@ def fetch_open_issues(owner, repo, since_days=30):
 
 def extract_issue_texts(issues):
     """Extract issue titles and descriptions from the list of issues."""
-    return [f"{issue['title']} {issue['body']}" for issue in issues]
+    return [f"{issue['title']} {issue['body']}" for issue in issues if 'pull_request' not in issue]
 
 def tokenize(text):
     return gensim.utils.simple_preprocess(remove_stopwords(text), deacc=True)
@@ -55,39 +56,51 @@ def compute_similarity_matrix(corpus):
     tfidf = models.TfidfModel(corpus)
     return similarities.MatrixSimilarity(tfidf[corpus])
 
-def find_duplicate_issues(issues, issue_texts, similarity_threshold=0.7):
+
+
+def find_duplicate_issues(issues, issue_texts, similarity_threshold=0.7, top_n=5):
     """Find and print duplicate issues based on the similarity threshold."""
     dictionary, corpus = create_corpus(issue_texts)
     index = compute_similarity_matrix(corpus)
 
-    num_issues = len(issues)
+    issue_texts_mapping = {i: issue for i, issue in enumerate(issues) if 'pull_request' not in issue}
+    index_mapping = list(issue_texts_mapping.keys())
+    num_issues = len(issue_texts)
     num_duplicates = 0
     similarity_sum = 0
     similarity_count = 0
     duplicate_pairs = set()
+    similarity_scores = []
+    top_pairs = []
 
     for i in tqdm(range(num_issues), desc="Processing issues"):
-        issue1 = issues[i]
+        issue1 = issue_texts_mapping[index_mapping[i]]
         similarities = index[corpus[i]]
-        for j, issue2 in enumerate(issues):
+        for j, issue2 in enumerate(issue_texts):
             if i != j:
                 similarity_sum += similarities[j]
                 similarity_count += 1
                 similarity_scores.append(similarities[j])
                 if similarities[j] > similarity_threshold:
-                    duplicate_pairs.add(tuple(sorted((issue1['number'], issue2['number']))))
-                    print(f"Issue {issue1['number']} and Issue {issue2['number']} might be duplicates with a similarity score of {similarities[j]:.2f}", flush=True)
+                    duplicate_pairs.add(tuple(sorted((issue1['number'], issue_texts_mapping[index_mapping[j]]['number']))))
+                    top_pairs.append(((issue1['number'], issue_texts_mapping[index_mapping[j]]['number']), similarities[j], issue1['html_url'], issue_texts_mapping[index_mapping[j]]['html_url']))
 
     mean_similarity = similarity_sum / similarity_count
     print(f"\nNumber of issues checked: {num_issues}")
     print(f"Number of possible duplicates found: {len(duplicate_pairs)}")
     print(f"Mean similarity: {mean_similarity:.2f}")
 
-    plt.hist(similarity_scores, bins=50)
-    plt.xlabel('Similarity score')
-    plt.ylabel('Frequency')
-    plt.title('Histogram of similarity scores')
-    plt.show()
+    # Get the top N duplicate pairs based on similarity scores
+    top_pairs = nlargest(top_n, top_pairs, key=lambda x: x[1])
+
+    print(f"\nTop {top_n} potential duplicate pairs:")
+    for pair in top_pairs:
+        print(f"Issue {pair[0][0]} and Issue {pair[0][1]} might be duplicates with a similarity score of {pair[1]:.2f}")
+        print(f"Link to Issue {pair[0][0]}: {pair[2]}")
+        print(f"Link to Issue {pair[0][1]}: {pair[3]}\n")
+
+
+
 if __name__ == '__main__':
     owner, repo = 'Torantulino', 'Auto-GPT'
     print("Fetching issues...")
