303b0996916f1c0d337850ae3bd80fda7ca35ec4
Toran Bruce Richards <toran.richards@gmail.com>
2023-04-24 19:50:40 +1200

    Runs black and isort

--- docs_generator.py
+++ docs_generator.py
@@ -1,12 +1,12 @@
-import os
-import sys
 import base64
+import os
 import re
+import sys
+
 import openai
+from dotenv import load_dotenv
 from github import Github
 from github.ContentFile import ContentFile
-from dotenv import load_dotenv
-import os
 from tqdm import tqdm  # Import the tqdm library
 
 load_dotenv()
@@ -17,6 +17,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # Initialize the API clients
 g = Github(GITHUB_API_KEY)
 openai.api_key = OPENAI_API_KEY
+
 
 def get_files_in_repo(repo):
     files = []
@@ -31,7 +32,10 @@ def get_files_in_repo(repo):
 
     return files
 
-def message_llm(system_prompt, user_prompt, model="gpt-4", temperature=0.7, max_tokens=3000):
+
+def message_llm(
+    system_prompt, user_prompt, model="gpt-4", temperature=0.7, max_tokens=3000
+):
     messages = [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": user_prompt},
@@ -46,28 +50,36 @@ def message_llm(system_prompt, user_prompt, model="gpt-4", temperature=0.7, max_
 
     return response.choices[0]["message"]["content"].strip()
 
+
 def summarize_file_gpt3(file_content):
     content = base64.b64decode(file_content.content).decode("utf-8")
     if len(content) > 4096:
         content = content[:4096]  # Truncate to 4096 tokens
 
     prompt = f"Summarize the following code:\n\n{content}\n\nSummary: "
-    summary = message_llm("Please provide a summary of the given code.", prompt, model="gpt-3.5-turbo")
+    summary = message_llm(
+        "Please provide a summary of the given code.", prompt, model="gpt-3.5-turbo"
+    )
 
     return summary
+
 
 def generate_readme_gpt4(summaries):
     prompt = "Generate detailed documentation for a project based on the following file summaries:\n\n"
     prompt += "\n".join(f"{i+1}. {summary}" for i, summary in enumerate(summaries))
-    readme_content = message_llm("Please generate detailed documentation for the project.", prompt, model="gpt-4")  # Replace with the actual GPT-4 model name when available
+    readme_content = message_llm(
+        "Please generate detailed documentation for the project.", prompt, model="gpt-4"
+    )  # Replace with the actual GPT-4 model name when available
 
     return readme_content
+
 
 def extract_repo_from_url(url):
     match = re.search(r"github.com/([\w-]+)/([\w-]+)", url)
     if match:
         return f"{match.group(1)}/{match.group(2)}"
     return None
+
 
 def main(repo_url):
     repo_path = extract_repo_from_url(repo_url)
@@ -81,7 +93,9 @@ def main(repo_url):
 
     print("Summarizing files...")
     summaries = []
-    for file in tqdm(files, desc="Processing Files"):  # Wrap the loop with tqdm progress bar
+    for file in tqdm(
+        files, desc="Processing Files"
+    ):  # Wrap the loop with tqdm progress bar
         summary = summarize_file_gpt3(file)
         summaries.append(summary)
 
@@ -93,6 +107,7 @@ def main(repo_url):
         readme_file.write(readme_content)
 
     print("README.md has been successfully generated!")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
