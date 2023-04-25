d9e85314092bc9bd038f481a5b21833352a4d254
Toran Bruce Richards <toran.richards@gmail.com>
2023-04-26 10:25:53 +1200

    tweak docgen prompts.

--- git-aid/docs_generator.py
+++ git-aid/docs_generator.py
@@ -72,15 +72,15 @@ def summarize_file_gpt3(file_content, file_name):
 
     prompt = f"Summarize the following file:\n\n{file_name}\n\n{content}"
     summary = message_llm(
-        "Please provide a summary of the given code.", prompt, model="gpt-3.5-turbo"
+        "Please provide a summary of the given file.", prompt, model="gpt-3.5-turbo"
     )
 
     return summary
 
 
 def generate_readme_gpt4(summaries):
-    system_prompt = "Your task is to write professional readmes to be displayed on GitHub projects. The user will provide you with a summary of every single file in the repository, pick the relevant information from these to create a well formatted and decorated readme. Ignore irrelevant files."
-    prompt = "Generate a detailed, professional and well decorated ReadMe for this GitHub project based on the following file summaries (ignore irrelevant files):\n\n"
+    system_prompt = "Your task is to write professional readmes to be displayed on the front page of GitHub projects. The user will provide you with a summary of every single file in the repository, including those which are irrelevant such as gitignores. Write a professional, well formatted and decorated GitHub readme."
+    prompt = "Please generate a professional GitHub Readme for this repository.\Repo Contents:\n"
     prompt += "\n".join(f"{i+1}. {summary}" for i, summary in enumerate(summaries))
     readme_content = message_llm(
         system_prompt=system_prompt, user_prompt=prompt, model="gpt-3.5-turbo"
