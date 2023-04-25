6e5358b27963d9501bc9a730774474a45b79e88e
Toran Bruce Richards <toran.richards@gmail.com>
2023-04-25 21:25:02 +1200

    Implements paralell processing.

--- docs_generator.py
+++ docs_generator.py
@@ -2,19 +2,20 @@ import base64
 import os
 import re
 import sys
+from concurrent.futures import ThreadPoolExecutor
 
 import openai
 from dotenv import load_dotenv
 from github import Github
 from github.ContentFile import ContentFile
-from tqdm import tqdm  # Import the tqdm library
+from tqdm import tqdm
+import time
+import openai.error
 
 load_dotenv()
-# Set your GitHub and OpenAI API keys
 GITHUB_API_KEY = os.getenv("GITHUB_READWRITE_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-# Initialize the API clients
 g = Github(GITHUB_API_KEY)
 openai.api_key = OPENAI_API_KEY
 
@@ -34,27 +35,34 @@ def get_files_in_repo(repo):
 
 
 def message_llm(
-    system_prompt, user_prompt, model="gpt-4", temperature=0.7, max_tokens=3000
+    system_prompt, user_prompt, model="gpt-4", temperature=0.7, max_tokens=3000, retries=5
 ):
     messages = [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": user_prompt},
     ]
 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature,
-        max_tokens=max_tokens,
-    )
-
-    return response.choices[0]["message"]["content"].strip()
-
+    for i in range(retries):
+        try:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                temperature=temperature,
+                max_tokens=max_tokens,
+            )
+            return response.choices[0]["message"]["content"].strip()
+        except openai.error.RateLimitError as e:
+            if i < retries - 1:
+                wait_time = 2 ** i
+                print(f"Rate limit exceeded. Retrying in {wait_time} seconds...")
+                time.sleep(wait_time)
+            else:
+                raise e
 
 def summarize_file_gpt3(file_content):
     content = base64.b64decode(file_content.content).decode("utf-8")
     if len(content) > 4096:
-        content = content[:4096]  # Truncate to 4096 tokens
+        content = content[:4096]
 
     prompt = f"Summarize the following code:\n\n{content}\n\nSummary: "
     summary = message_llm(
@@ -68,8 +76,8 @@ def generate_readme_gpt4(summaries):
     prompt = "Generate detailed documentation for a project based on the following file summaries:\n\n"
     prompt += "\n".join(f"{i+1}. {summary}" for i, summary in enumerate(summaries))
     readme_content = message_llm(
-        "Please generate detailed documentation for the project.", prompt, model="gpt-4"
-    )  # Replace with the actual GPT-4 model name when available
+        "Please generate detailed documentation for the project.", prompt, model="gpt-3.5-turbo"
+    )
 
     return readme_content
 
@@ -93,17 +101,21 @@ def main(repo_url):
 
     print("Summarizing files...")
     summaries = []
-    for file in tqdm(
-        files, desc="Processing Files"
-    ):  # Wrap the loop with tqdm progress bar
-        summary = summarize_file_gpt3(file)
-        summaries.append(summary)
+    with ThreadPoolExecutor() as executor:
+        summaries = list(tqdm(executor.map(summarize_file_gpt3, files), total=len(files), desc="Processing Files"))
 
     print("Generating README.md content...")
     readme_content = generate_readme_gpt4(summaries)
 
     print("Writing README.md to disk...")
-    with open("output/README.md", "w") as readme_file:
+
+    # Create the output directory if it doesn't exist
+    output_dir = "./output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Write the README.md file to the output directory
+    with open(os.path.join(output_dir, "README.md"), "w") as readme_file:
         readme_file.write(readme_content)
 
     print("README.md has been successfully generated!")
